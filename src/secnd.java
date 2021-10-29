2fe85f24fcbe31efdd96acb6c975115ceaf4ec7b
kiseki <kisekidatte@gmail.com>
2021-10-29 13:21:39 +0900

    Initial commit

---
+++ src/secnd.java
@@ -0,0 +1,12 @@
+public class secnd {
+    public static void main(String[] args ) {
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i*3;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+
+}
---
+++ src/thrd.java
@@ -0,0 +1,13 @@
+public class thrd {
+    public static void main(String[] args ) {
+        int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<6){
+                arr[i]=arr[i]*2;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
